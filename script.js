3302a16d6b167010ea9e8f360cbb6f3f9babab8a
Hamza Tariq <129800852+pakoramaster@users.noreply.github.com>
2025-01-01 22:20:02 -0500

    added basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-"" 
+// Calculator
+
+
+const display = document.getElementById("display");
+
+function appendValue(input) {
+    display.value += input;
+}
+
+function clear() {
+    display.value= "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
